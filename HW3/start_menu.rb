40aaef5c653f353cb43948418ce28992d956d28e
Anton921 <62765431+Anton921@users.noreply.github.com>
2021-12-04 19:51:45 +0200

    Add files via upload

---
+++ HW3/start_menu.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# В цьому файлі організоване початкове меню.
+require_relative 'dog_or_cat'
+include Dog_or_Cat
+
+module Start
+  def start_end
+    z = 1
+    while z != 2
+      puts('Грати(1)')
+      puts('Вийти(2)')
+      z1 = gets.chop
+
+      case z1
+      when '1'
+        play
+      when '2'
+        z = 2
+      else
+        puts('Ви ввели неправельний символ, спробуйте ще')
+      end
+    end
+  end
+end
